cfbfc5fe8b8f864af30cf32b18e1650aa058656e
Utpal Kumar <utpalkumar50@gmail.com>
2024-11-07 17:42:28 -0800

    realtime data viz accel

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_socketio import SocketIO
 import socket
 import json
 import threading
-from collections import deque
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -11,9 +10,6 @@ socketio = SocketIO(app)
 # Set up TCP server configuration
 server_ip = '0.0.0.0'
 port = 56204
-
-# Initialize a deque with a fixed maximum length
-data_buffer = deque(maxlen=1000)
 
 def start_tcp_server():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -40,10 +36,7 @@ def start_tcp_server():
                     
                     try:
                         json_data = json.loads(json_str)
-                        # Store the received data in the deque
-                        data_buffer.append(json_data)
-                        
-                        # Emit the latest data to the frontend
+                        # Emit the latest data to the frontend without storing
                         socketio.emit('new_data', json_data)
                     except json.JSONDecodeError:
                         continue
@@ -51,12 +44,6 @@ def start_tcp_server():
 @app.route('/')
 def index():
     return render_template('index.html')
-
-# Endpoint to fetch all current data in the deque for initial load
-@socketio.on('fetch_all_data')
-def handle_fetch_all_data():
-    # Convert the deque to a list and send to frontend
-    socketio.emit('all_data', list(data_buffer))
 
 if __name__ == '__main__':
     # Start TCP server in a separate thread
